2ebd75adb024727bcf601cc525732885f510d491
unknown <msvbnet@hotmail.com>
2019-04-09 04:27:38 +0200

    9 Apri 2019

--- VazorPages1/Pages/Index.cshtml.vb
+++ VazorPages1/Pages/Index.cshtml.vb
@@ -6,10 +6,13 @@ Public Class IndexModel : Inherits PageModel
     ' This property is used in the Index.cshtml,
     ' to inject our vbxml code in the page as a pratial view
 
+    Const Title = "Index"
+
     Public ReadOnly Property ViewName As String
         Get
+            ViewData("Title") = Title
             Dim html = GetVbXml(Students, ViewData).ParseTemplate(Students)
-            Return VazorPage.CreateNew("Index", "Pages", "Vazor MVC", html)
+            Return VazorPage.CreateNew("Index", "Pages", Title, html)
         End Get
     End Property
 
